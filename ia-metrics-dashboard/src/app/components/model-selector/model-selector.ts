130317e50f1678231899f5a2b85411afa1279dba
Jhon-Alv <jhon1_alvarado@outlook.com>
2026-07-17 18:10:00 -0500

    fix: actualizar docker-compose con nuevos nombres de directorios (frontend-catalogo, frontend-dashboard)

--- ia-metrics-dashboard/src/app/components/model-selector/model-selector.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-model-selector',
-  imports: [],
-  templateUrl: './model-selector.html',
-  styleUrl: './model-selector.css',
-})
-export class ModelSelector {}
--- ia-metrics-dashboard/src/app/components/real-time-chart/real-time-chart.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-real-time-chart',
-  imports: [],
-  templateUrl: './real-time-chart.html',
-  styleUrl: './real-time-chart.css',
-})
-export class RealTimeChart {}
